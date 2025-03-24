f590c04e18242aa3c3b59dbc8b514b063f991e18
quequuen <150333112+quequuen@users.noreply.github.com>
2025-03-24 17:06:06 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    if(n===1)
+        return 1;
+    if(n){
+        
+    }
+    if(n%6===0){ //n이 6의 배수일 때
+            return n/6;
+        }else{  //n이 6의 배수가 아닐 때
+            for(let i=1;i<=n;i++){
+                if((6*i)%n===0){
+                    return i; 
+                }
+            }
+        }
+}
