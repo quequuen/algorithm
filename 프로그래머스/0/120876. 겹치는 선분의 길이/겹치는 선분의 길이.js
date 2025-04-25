5e1130e1143bf66f201f2e83b36165da5599db1f
quequuen <150333112+quequuen@users.noreply.github.com>
2025-04-25 22:09:06 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 4.11 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -0,0 +1,19 @@
+function solution(lines) {
+    
+    let object = {};
+    let count = 0;
+    for(let [a,b] of lines){
+        for(let i=a+1;i<=b;i++){
+            object[i]=(object[i]+1||0);
+        }
+    }
+    console.log(object);
+    for(let key in object){
+        if(object[key]>0){
+            count++;
+        }
+    }
+    
+    
+    return count;
+}
