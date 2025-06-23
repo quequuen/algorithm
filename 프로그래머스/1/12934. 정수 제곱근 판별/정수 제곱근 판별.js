455184ac29bf6b12131614766a00183f9f46aea7
quequuen <150333112+quequuen@users.noreply.github.com>
2025-06-24 03:31:56 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 29.79 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,16 +1,15 @@
 function solution(n) {
-    let half = Math.floor(n);
     let answer = 0;
-    for(let i =1;i<=half;i++){
+    let i = 1;
+    while(i<=n){
         if(i**2===n){
             answer = i;
             break;
         }
         else{
-           answer = -1;
-        };
-        
+            answer = -1;
+        }
+        i++;
     }
-    
     return answer===-1?answer:(++answer)**2;
 }
